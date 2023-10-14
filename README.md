0a524da1be541ea7a0b60b5223a9279a9f637e9f
kai <kwen2921@gmail.com>
2023-10-14 06:24:39 -0400

    adds README.md; fixes multiple correct answers issue

---
+++ README.md
@@ -0,0 +1,38 @@
+# Trivia Quest 
+
+## Overview:
+
+![alt text](https://github.com/manyprojects/hackathon-trivia-quest/blob/kai-branch-1/assets/trivia.jpg)
+
+Welcome to Trivia Quest, a collaborative effort of three skilled developers aimed at tackling a Trivia Quest app. In this README, we'll provide an overview of the project, its objectives, and how to get started contributing as our 5-hour Hackathon at BrainStation.
+
+## Project Goals: 
+
+This project fetches trivia data from jService /Clues with Axios. The functionalities are based on the questions and answers from the data interacting with users' answers to each trivia question. 
+
+
+## Team Members:
+
+- Sonia
+- Giray
+- Kai
+
+## Prerequisites:
+
+- API: http://jservice.io/ ,DOM (Document Object Model), Axios
+- IDE: Visual Studio Code, Google Chrome Browser
+- Language: HTML, Sass, CSS
+- Framework: Vanilla JS (Vanilla Javascript)
+
+## Uuage: 
+ Open 'index.html' with broswer to start Trivia Quest
+
+
+## Acknowledgments:
+
+We want to express our gratitude to the BrainStation community for their support and inspiration. This project wouldn't be possible without the contributions of our team members.
+
+
+
+
+
--- javascript/game-page.js
+++ javascript/game-page.js
@@ -1,47 +1,73 @@
 const button = document.querySelector(".question__button");
 const answer = document.querySelector(".main__answer-box");
-
 const answerA = document.querySelector(".main__answer-1");
 const answerB = document.querySelector(".main__answer-2");
 const answerC = document.querySelector(".main__answer-3");
 const answerD = document.querySelector(".main__answer-4");
-
 const arrAnswer = [answerA, answerB, answerC, answerD];
+let response = null;
 
 
-button.addEventListener("click", function(e) {
-    // e.preventDefault();
-    const displayQuestion = async () => {
-        try {
-            const response = await axios.get("http://jservice.io/api/clues");
-            let randomIndex = Math.floor(Math.random()*100);
-            let answerIndex = Math.floor(Math.random()*4);
+const displayQuestion = async () => {
+    try {
+        response = await axios.get("http://jservice.io/api/clues");
+        let randomIndex = Math.floor(Math.random()*100);
+        let answerIndex = Math.floor(Math.random()*4);
+        resetQuestion();
+        function resetQuestion () {
+            randomIndex = Math.floor(Math.random()*100);
+            answerIndex = Math.floor(Math.random()*4);
             document.querySelector(".main__question").textContent = response.data[randomIndex].question;
-            // answer section
             arrAnswer[answerIndex].textContent = response.data[randomIndex].answer;
-            // console.log(arrAnswer[answerIndex].textContent);
             for (let i = 0; i < 4; i++) {
-                if (i !== answerIndex) {
-                    arrAnswer[i].textContent = response.data[Math.floor(Math.random()*100)].answer;
-                    console.log(arrAnswer[i]);
-                }
-                else {
-                    // do nothing
+                if (i !== answerIndex) {   
+                    arrAnswer[i].textContent = response.data[Math.floor(Math.random()*100)].answer;               
                 }
             }
-            // answer section
-            answer.addEventListener("click", function(event) {
-            // event.preventDefault();
-                if (event.target.textContent === arrAnswer[answerIndex].textContent) {
-                    event.target.textContent = "Correct!";
-                }
-                else {
-                    event.target.textContent = "Wrong! Try again!";
-                }
-            });
-        } catch(e) {
-            console.log(e);
         }
+
+        button.addEventListener("click", event => {resetQuestion()});
+        answerA.addEventListener("click", event => {
+            event.preventDefault();
+            if (event.target.textContent === arrAnswer[answerIndex].textContent) {
+                event.target.textContent = "Correct!";
+                button.addEventListener("click", event => {resetQuestion()});
+            } else {
+                event.target.textContent = "Wrong! Try again!";
+            }
+        });
+        answerB.addEventListener("click", function(event) {
+            event.preventDefault();
+            if (event.target.textContent === arrAnswer[answerIndex].textContent) {
+                event.target.textContent = "Correct!";
+                button.addEventListener("click", event => {resetQuestion()});
+            } else {
+                event.target.textContent = "Wrong! Try again!";
+            }
+
+        });
+        answerC.addEventListener("click", function(event) {
+            event.preventDefault();
+            if (event.target.textContent === arrAnswer[answerIndex].textContent) {
+                event.target.textContent = "Correct!";
+                button.addEventListener("click", event => {resetQuestion()});
+            } else {
+                event.target.textContent = "Wrong! Try again!";
+            }
+        });
+        answerD.addEventListener("click", function(event) {
+            event.preventDefault();
+            if (event.target.textContent === arrAnswer[answerIndex].textContent) {
+                event.target.textContent = "Correct!";
+                button.addEventListener("click", event => {resetQuestion()});
+            } else {
+                event.target.textContent = "Wrong! Try again!";
+            }
+        });
+
+    } catch(e) {
+        console.log(e);
     }
-    displayQuestion();
-});
+};
+
+displayQuestion();
